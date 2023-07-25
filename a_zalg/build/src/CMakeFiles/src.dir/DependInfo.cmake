50e3521ff67ca76b1352e7fd756afdc5794f65bd
nxing21 <82045107+nxing21@users.noreply.github.com>
2023-07-24 22:58:11 -0500

    Add files via upload

---
+++ a_zalg/build/src/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/cs225/.devcontainer/release/src/naive.cpp" "src/CMakeFiles/src.dir/naive.cpp.o" "gcc" "src/CMakeFiles/src.dir/naive.cpp.o.d"
+  "/workspaces/cs225/.devcontainer/release/src/zalg.cpp" "src/CMakeFiles/src.dir/zalg.cpp.o" "gcc" "src/CMakeFiles/src.dir/zalg.cpp.o.d"
+  "/workspaces/cs225/.devcontainer/release/src/zval.cpp" "src/CMakeFiles/src.dir/zval.cpp.o" "gcc" "src/CMakeFiles/src.dir/zval.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspaces/cs225/.devcontainer/release/build/lib/CMakeFiles/cs225.dir/DependInfo.cmake"
+  "/workspaces/cs225/.devcontainer/release/build/lib/CMakeFiles/lodepng.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ a_zalg/build/src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/naive.cpp.o"
+  "CMakeFiles/src.dir/naive.cpp.o.d"
+  "CMakeFiles/src.dir/zalg.cpp.o"
+  "CMakeFiles/src.dir/zalg.cpp.o.d"
+  "CMakeFiles/src.dir/zval.cpp.o"
+  "CMakeFiles/src.dir/zval.cpp.o.d"
+  "libsrc.a"
+  "libsrc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
